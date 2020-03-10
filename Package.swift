af891de54b49cfbb3125dfa87a0b7278447a74a3
Mounir Dellagi <mounir.dellagi@wallapop.com>
2020-03-10 11:31:08 +0100

    SPM Package.swift file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "AFNetworking",
+    platforms: [.iOS(.v10)],
+    // platforms: [.iOS("10.0")],
+    products: [
+        .library(name: "AFNetworking", targets: ["AFNetworking iOS"])
+    ],
+    targets: [
+        .target(
+            name: "AFNetworking",
+            path: "AFNetworking"
+        )
+    ]
+)
